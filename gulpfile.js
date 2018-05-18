301dec29de7372bea26bacd6acb817b8630dba33
João Luiz Schlickmann Kestering <jlsk25@gmail.com>
2018-05-18 11:10:54 -0300

    Creating documentation for Ionic app

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp");
+var connect = require("gulp-connect");
+var typedoc = require("gulp-typedoc");
+var config = {
+  root: "docs/"
+};
+
+gulp.task("typedoc", function() {
+  return gulp.src(["src/**/*.ts"]).pipe(
+    typedoc({
+      module: "commonjs",
+      target: "es6",
+      experimentalDecorators: true,
+      out: config.root,
+      name: "DocApp",
+      readme: "./README.md"
+    })
+  );
+});
+
+gulp.task("serve:docs", ["typedoc"], function() {
+  connect.server({
+    root: config.root,
+    port: 8090,
+    livereload: true
+  });
+});
